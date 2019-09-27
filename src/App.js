70ec2ed38057ed867fa608f8541a4dbf2641a236
franiczio <michal.fr@nelak.pl>
2019-09-27 20:34:09 +0200

    git init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card, CardContent, CardHeader, Typography } from '@material-ui/core';
+
+function App() {
+  return (
+    <Card>
+      <CardHeader title="Books app" />
+      <CardContent>
+        <p>Hello?</p>
+        <Typography>Hello books!</Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default App;
