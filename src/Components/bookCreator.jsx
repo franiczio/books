1adcce6c77ab4729a01f68d15d27d716b82647a8
franiczio <michal.fr@nelak.pl>
2019-09-29 14:02:26 +0200

    bookCreator component init

---
+++ src/Components/bookCreator.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+
+class BookCreator extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Fragment></Fragment>;
+  }
+}
+
+export default BookCreator;
