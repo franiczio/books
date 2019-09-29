b3aeff797c7851ecac50737d834eee30361ec253
franiczio <michal.fr@nelak.pl>
2019-09-29 12:43:33 +0200

    bookComponentTemplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Card, CardContent, CardHeader, Typography } from '@material-ui/core';
-
+import Book from './Components/book.jsx';
 function App() {
   return (
     <Card>
       <CardHeader title="Books app" />
       <CardContent>
-        <p>Hello?</p>
-        <Card>Title:</Card>
+        <Book />
         <Typography>Hello books!</Typography>
       </CardContent>
     </Card>
