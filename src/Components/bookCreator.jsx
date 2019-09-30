c05d7959217c76ddf5a36359c67861aa8659f75c
franiczio <michal.fr@nelak.pl>
2019-09-30 20:08:38 +0200

    sending post request creates new book

--- src/Components/bookCreator.jsx
+++ src/Components/bookCreator.jsx
@@ -1,11 +1,32 @@
 import React, { Component, Fragment } from 'react';
 import * as bookActions from '../Actions/bookAction';
 import { ReactReduxContext, connect } from 'react-redux';
+import axios from 'axios';
 
 class BookCreator extends Component {
   constructor(props) {
     super(props);
+    this.create = this.create.bind(this);
   }
+
+  create = event => {
+    let title = event.target[1].value;
+    let isbn = event.target[3].value;
+    let description = event.target[5].value;
+    let author = event.target[7].value;
+    let publicationDate = event.target[9].value;
+    var requestBody = {
+      isbn: isbn,
+      title: title,
+      description: description,
+      author: author,
+      publicationDate: publicationDate,
+      reviews: [],
+    };
+    console.log(requestBody);
+    axios.post('https://demo.h88.dev/books', requestBody).then(resp => {});
+    this.goBackToBooksSite();
+  };
 
   goBackToBooksSite = () => {
     this.props.history.push('/');
@@ -19,11 +40,14 @@ class BookCreator extends Component {
           <a onClick={this.goBackToBooksSite}>BACK</a>
         </div>
         <div>
-          <form>
+          <form onSubmit={this.create}>
             <div>
               <fieldset>
                 <legend>title</legend>
-                <input placeholder="title"></input>
+                <input
+                  ref={title => (this.el = title)}
+                  placeholder="title"
+                ></input>
               </fieldset>
             </div>
             <div>
@@ -50,7 +74,7 @@ class BookCreator extends Component {
                 <input placeholder="12-21-2019"></input>
               </fieldset>
             </div>
-            <button>CREATE</button>
+            <button type="submit">CREATE</button>
           </form>
         </div>
       </Fragment>
