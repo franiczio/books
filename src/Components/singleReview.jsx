7908cba273421894890c1e81416ec19095187269
franiczio <michal.fr@nelak.pl>
2019-10-01 20:59:59 +0200

    delete reviews added

--- src/Components/singleReview.jsx
+++ src/Components/singleReview.jsx
@@ -1,8 +1,21 @@
 import React, { Component, Fragment } from 'react';
+import * as bookActions from '../Actions/bookAction';
+import * as reviewActions from '../Actions/reviewAction';
+import { ReactReduxContext, connect } from 'react-redux';
+import axios from 'axios';
+
 class SingleReview extends Component {
   constructor(props) {
     super(props);
+    this.deleteReviewFromServer = this.deleteReviewFromServer.bind(this);
   }
+
+  deleteReviewFromServer = () => {
+    let getPath = 'https://demo.h88.dev' + this.props.id;
+    axios.delete(getPath).then(resp => {});
+    this.props.resetReview(this.props.id);
+  };
+
   render() {
     return (
       <Fragment>
@@ -12,12 +25,24 @@ class SingleReview extends Component {
           <td>{this.props.author}</td>
           <td>{this.props.publicationDate}</td>
           <td>
-            <button>DELETE</button>
+            <button onClick={this.deleteReviewFromServer}>DELETE</button>
           </td>
         </tr>
       </Fragment>
     );
   }
 }
-
-export default SingleReview;
+const mapStateToProps = state => {
+  return state;
+};
+const mapActionsToProps = {
+  resetReview: reviewActions.resetOneReview,
+  getBook: bookActions.getOneBook,
+  resetBooks: bookActions.resetAllBooks,
+  resetReviews: reviewActions.resetAllReviews,
+  getAllReviews: reviewActions.getAllReviews,
+};
+export default connect(
+  mapStateToProps,
+  mapActionsToProps,
+)(SingleReview);
